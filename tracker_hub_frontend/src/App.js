2276a33fd24deba3f8883e6cc5d845ced6a6e14f
Naveen kumar Kalyanam <naveenkumarkalyanam@Naveens-Air.lan>
2023-02-01 00:22:05 -0500

    User story3

--- tracker_hub_frontend/src/App.js
+++ tracker_hub_frontend/src/App.js
@@ -2,6 +2,9 @@ import Navbar from "./Pages/Navbar/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signup from "./Pages/Signup/Signup";
 import Login from "./Pages/Login/Login";
+import Profile from "./Pages/Profile/Profile";
+import EditProfile from "./Pages/Profile/EditProfile";
+
 function App() {
   return (
     <div className="App" >
@@ -10,6 +13,8 @@ function App() {
         <Routes>
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+              <Route path="profile" element={<Profile />} />
+              <Route path="editprofile" element={<EditProfile />} />
           </Route>
         </Route>
         <Route path="*"  element={<Notfound />}/>
